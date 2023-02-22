e52d18fe55652dee77e9ab0baf206cdb15d08ee1
prashant <k.prashant093@gmail.com>
2023-02-22 09:13:14 +0530

    chapter 1: favour composition over inheritance

--- src/chapter1/Main.java
+++ src/chapter1/Main.java
@@ -1,6 +1,7 @@
 package chapter1;
 
 import chapter1.abstractType.Duck;
+import chapter1.services.impl.FlyRocketPowered;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +10,12 @@ public class Main {
         mallardDuck.performQuack();
         mallardDuck.performFly();
         mallardDuck.swim();
+
+        Duck modelDuck = new ModelDuck();
+        modelDuck.display();
+        modelDuck.performQuack();
+        modelDuck.performFly();
+        modelDuck.setFlyBehaviour(new FlyRocketPowered());
+        modelDuck.performFly();
     }
 }
--- src/chapter1/abstractType/Duck.java
+++ src/chapter1/abstractType/Duck.java
@@ -20,4 +20,12 @@ public abstract class Duck {
     public void performFly() {
         flyBehaviour.fly();
     }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    public  void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
 }
